3cc9dcefc6aa68e04537f2d4b26ab4af75c2713f
HARRY AMOAFO FYNN <109153581+streetprophet90@users.noreply.github.com>
2024-07-31 14:31:04 +0000

    urls path defined

--- gift_platform/urls.py
+++ gift_platform/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from gift_platform.users import views as user_views
+from gift_platform.wishlist import views as wishlist_views
+from gift_platform.orders import views as order_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('wishlist/', wishlist_views.wishlist, name='wishlist'),
+    path('wishlist/add/', wishlist_views.add_item, name='add_item'),
+    path('order/<int:item_id>/', order_views.place_order, name='place_order'),
 ]
